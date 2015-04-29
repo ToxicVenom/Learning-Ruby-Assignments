2df7726c2beddd5166d682def9df0afa14c1bbd7
ToxicVenom <phillipcoetzee14@gmail.com>
2015-04-29 16:40:08 +0100

    modify .rb file

--- Work Promt 1 - Game loops and user input/User input and response.rb
+++ Work Promt 1 - Game loops and user input/User input and response.rb
@@ -3,7 +3,8 @@ user_input = gets.chomp
 user_input.downcase!
  while user_input != ""
  puts "Nice to meet you!"
- if user_input == ""
-   puts "No, you have to be someone, anyone!"
+end
+if user_input == ""
+  puts "No, you have to be somebody."
 end
 end
