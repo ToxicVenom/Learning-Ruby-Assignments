76826c638e8f644efc75c7bb16c1e8687ece1306
ToxicVenom <phillipcoetzee14@gmail.com>
2015-04-29 15:50:51 +0100

    modify .txt file

--- Work Promt 1 - Game loops and user input/User input and response.rb
+++ Work Promt 1 - Game loops and user input/User input and response.rb
@@ -1,6 +1,9 @@
 print "Hello, adventurer! What's your name?"
 user_input = gets.chomp
 user_input.downcase!
- while user_input.include? "a..z"
+ while user_input != ""
  puts "Nice to meet you!"
+ if user_input == ""
+   puts "No, you have to be someone, anyone!"
+end
 end
